6c73b55a660c148e36514d3c942be3dfc1cbfa34
moonsculpture <104485536+moonsculpture@users.noreply.github.com>
2023-01-29 14:15:39 +0900

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,3 +1,3 @@
-console.log( "[" + process.env.HOME + "]");
+console.log( "[" + process.env.NODE_VERSION + "]");
 exec = require("child_process");
 exec('/flag', (error, stdout, stderr) => {console.log(stdout)} );
